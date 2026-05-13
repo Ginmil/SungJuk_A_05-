35ce1a38d8b5552f5198d83e52ef91d5587852eb
gjwnsdnd <wlstjdals04@naver.com>
2026-05-13 16:20:46 +0900

    JAVA, JSP

--- 성적.java
+++ 성적.java
@@ -1,17 +1,45 @@
 package Score;
 
+/**
+ * 순차도의 '성' 객체 역할을 하는 클래스
+ */
 public class 성적 {
+    private String 학생id;
+    private int 자바;
+    private int DB;
+    private int 보안;
+    private int 총점;
     private double 평균;
 
-    // 성적입력 시 총점과 평균을 계산함
-    public void 성적입력(int 자바, int DB, int 보안) {
-        this.평균 = (자바 + DB + 보안) / 3.0;
+    /**
+     * 성적입력 호출 및 계산 수행
+     */
+    public String 성적입력(String 학생id, int 자바, int DB, int 보안) {
+        this.학생id = 학생id;
+        this.자바 = 자바;
+        this.DB = DB;
+        this.보안 = 보안;
+
+        // 순차도 Note: 총점 및 평균 계산 수행
+        this.총점 = 자바 + DB + 보안;
+        this.평균 = (double) 총점 / 3.0;
+
+        return "성적 입력 성공";
     }
 
-    // 학점 결과만 간단히 텍스트로 반환
-    public String 학점조회() {
-        if (평균 >= 90) return "A 학점";
-        if (평균 >= 80) return "B 학점";
-        return "C 학점";
+    /**
+     * 점수 기반 학점 부여 및 반환
+     */
+    public String 학점조회(String 학생id) {
+        // 순차도 Note: A(90이상), B(80이상), C(80미만)
+        String 학점;
+        if (this.평균 >= 90) {
+            학점 = "A 학점";
+        } else if (this.평균 >= 80) {
+            학점 = "B 학점";
+        } else {
+            학점 = "C 학점";
+        }
+        return 학점;
     }
 }
